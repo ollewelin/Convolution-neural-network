14f5fb61c36591644b376a242d7a4d54caef3758
Olle Welin <olle_welin@hotmail.com>
2017-11-30 21:51:16 +0100

    Create c_func.h

---
+++ c_func.h
@@ -0,0 +1,7 @@
+#ifndef C_FUNC_H_INCLUDED
+#define C_FUNC_H_INCLUDED
+#include <math.h>
+#include "c_func.c"
+float abs_value(float signed_value);
+int get_CIFAR_file_size(void);
+#endif // C_FUNC_H_INCLUDED
